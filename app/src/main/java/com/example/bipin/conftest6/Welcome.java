7eef2fd81b6e5a2c90c0d978782b65bda6b941f0
bipindr123 <bipindr123@gmail.com>
2018-06-20 22:48:54 +0530

    Added initial navigation drawer

--- app/src/main/java/com/example/bipin/conftest6/Welcome.java
+++ app/src/main/java/com/example/bipin/conftest6/Welcome.java
@@ -1,9 +1,12 @@
 package com.example.bipin.conftest6;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,16 @@ import com.example.bipin.conftest6.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.example.bipin.conftest6.LoginActivity;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class Welcome extends AppCompatActivity {
 
@@ -25,6 +38,52 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("home");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("events");
+
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.escilogo)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Bipin").withEmail("bipindr123@gmail.com").withSelectedColor(Color.BLUE)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+
+
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        item1,item2, new DividerDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Log.d("check", "onItemClick: "+ position + drawerItem);
+                        if (drawerItem.getIdentifier() == 2) {
+                            // load tournament screen
+                            Intent intent = new Intent(Welcome.this, MainActivity2.class);
+
+                            startActivity(intent);
+                        }
+                        return true;
+                    }
+                })
+                .build();
         logoutBtn = (Button) findViewById(R.id.logoutBtn);
 
 
@@ -61,6 +120,7 @@ public class Welcome extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
 
         mAuth.addAuthStateListener(mAuthListener);
     }
